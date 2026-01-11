ef2d0372c58c9521000ef0816c1126cc83038ab6
WIZ/Aditya <pro.aditya.r.com>
2026-01-11 12:10:35 +0530

    bruh

--- src/script/mapping.py
+++ src/script/mapping.py
@@ -1,32 +1,45 @@
-# temporary until i add syllabel based mapping
-GLYPH_MAP = {
-    'a': 'a.svg',
-    'b': 'ba.svg',
-    'c': 'cha.svg',
-    'd': 'da.svg',
-    'e': 'e.svg',
-    'f': 'fha.svg',
-    'g': 'ga.svg',
-    'h': 'ha.svg',
-    'i': 'i.svg',
-    'j': 'ja.svg',
-    'k': 'ka.svg',
-    'l': 'la.svg',
-    'm': 'ma.svg',
-    'n': 'na.svg',
-    'o': 'o.svg',
-    'p': 'pa.svg',
-    'q': 'ka.svg', 
-    'r': 'ra.svg',
-    's': 'sa.svg',
-    't': 'ta.svg',
-    'u': 'u.svg',
-    'v': 'va.svg',
-    'w': 'va.svg',
-    'x': 'k.svg', 
-    'y': 'ya.svg',
-    'z': 'z.svg',
-    ' ': 'space',
+# https://en.wikipedia.org/wiki/Help:IPA/English
+# changing to phonemes for mapping
+'''
+CONSONANTS = 
+    'b': 'ब', 'd': 'ड', 'f': 'फ़', 'g': 'ग', 'h': 'ह', 'ʤ': 'ज', 'k': 'क', 
+    'l': 'ल', 'm': 'म', 'n': 'न', 'p': 'प', 'r': 'र', 's': 'स', 't': 'ट', 
+    'v': 'व', 'w': 'व', 'z': 'ज़', 'ʒ': 'झ़', 'tʃ': 'च', 'ʃ': 'श', 'θ': 'थ', 
+    'ð': 'द', 'ŋ': 'ङ', 'j': 'य', 'ɡ': 'ग' # ɡ is a variant of g
+
+VOWELS =
+    'ə': ('अ', ''),     'e': ('ए', 'े'),     'æ': ('ऐ', 'ै'), 
+    'ɪ': ('इ', 'ि'),    'i': ('ई', 'ी'),     'iː': ('ई', 'ी'),
+    'ɒ': ('ऑ', 'ॉ'),    'ɔ': ('ऑ', 'ॉ'),     'ʊ': ('उ', 'ु'), 
+    'u': ('ऊ', 'ू'),    'uː': ('ऊ', 'ू'),    'ʌ': ('अ', ''), 
+    'ɑ': ('आ', 'ा'),    'ɑː': ('आ', 'ा'),
+    
+    # Diphthongs (Gliding vowels)
+    'aɪ': ('आइ', 'ाइ'), 'eɪ': ('एइ', 'ेइ'),  'ɔɪ': ('ऑइ', 'ॉइ'),
+    'aʊ': ('आउ', 'ाउ'), 'oʊ': ('ओ', 'ो'),    'əʊ': ('ओ', 'ो')
+
+'''
+VOWELS={
+    'ɑ':'a.svg',
+    'ɔ':'o.svg',
+    'o':'o,svg',
+    'æ':'i.svg',
+    'aɪ':'i.svg'
+    
+
+
+}
+
+CONSONANTS = {
+    ' ': 'space',    'b': 'ba.svg',    'd': 'da.svg',
+    'ʤ': 'ja.svg',    'ð': 'dha.svg',    'f': 'fha.svg',
+    'g': 'ga.svg',    'h': 'ha.svg',    'w':'va.svg',#hw also
+    'j':'ya.svg',    'k':'ka.svg',    'l':'la.svg',
+    'm':'ma.svg',    'n':'na.svg',    'ŋ':'nga.svg',
+    'p':'pa.svg',    'r':'ra.svg',    's':'sa.svg',
+    'ʃ':'shey.svg',#श
+    't':'ta.svg',    'ʧ':'cha.svg',    'θ':'ttha.svg',
+    'v':'va.svg',    'z':'z.svg',    'ʒ':'jha.svg',
 }
 
 def get_glyph_filename(char: str):
